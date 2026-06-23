f8eba76dd1d0da79e2ef874136a34b848502c5bd
Aditya Sharma <adityas20032005@gmail.com>
2026-06-23 08:37:46 +0530

    Sign Up pafe

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -4,24 +4,28 @@ import { useState } from 'react'
 import { LabelledInput } from './LabelledInput'
 import axios from 'axios'
 import LoaderAnim from './LoaderAnim'
+import { signup } from '@/services/auth.service'
 
 const BACKEND_URL = "http://127.0.0.1:8787";
 
 const SignUpForm = () => {
     const [loading,setLoading] = useState(false);
     const navigate = useNavigate();
-    const [postInput,setPostInput] = useState<SignUpSchema>({
-        email:"",
-        password:"",
-        name:""
-    })
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [username,setUserName] = useState("");
+    
 
-    async function sendRequest(){
+    async function handleSubmit(){
         try{
             setLoading(true);
-            const response = await axios.post(`${BACKEND_URL}/api/v1/user/signup`,postInput)
-            const jwt = response.data.jwt;
-            localStorage.setItem("token",jwt)
+            const data = await signup(
+                name,
+                username,
+                password,
+                email
+            )
             navigate("/blogs")
         }
         catch(e:any){
@@ -37,25 +41,20 @@ const SignUpForm = () => {
             <div className='text-4xl font-extrabold mb-2 px-6'>Let's Get Started</div>
             <div className='text-md text-gray-500 mb-2'>Already Have An Account? <Link to={'/signin'} className='underline font-semibold'>Login</Link></div>
        
-        <LabelledInput label='Username' placeholder='Enter Your Name' onchange={(e)=>{
-                setPostInput({
-                    ...postInput,
-                    name: e.target.value
-                })
+        <LabelledInput label='Username' placeholder='Enter Your UserName' onchange={(e)=>{
+               setUserName(e.target.value);
+            }}/>  
+        <LabelledInput label='Name' placeholder='Enter Your Name' onchange={(e)=>{
+                setName(e.target.value);
+                
             }}/>  
         <LabelledInput label='Email' type="email" placeholder='Enter Your Email' onchange={(e)=>{
-            setPostInput({
-                ...postInput,
-                email: e.target.value
-            })
+            setEmail(e.target.value)
         }}/>
         <LabelledInput label='Password' type='password' placeholder='Enter Your Password' onchange={(e)=>{
-            setPostInput({
-                ...postInput,
-                password: e.target.value
-            })
+            setPassword(e.target.value)
         }}/>
-        <button onClick={sendRequest} type="button" className="mt-5 w-full text-white bg-gray-950 box-border border border-transparent hover:shadow-lg hover:bg-black focus:ring-4 focus:ring-neutral-tertiary shadow-lg font-medium leading-5 rounded-lg text-md px-4 py-2.5 focus:outline-none">
+        <button onClick={handleSubmit} type="button" className="mt-5 w-full text-white bg-gray-950 box-border border border-transparent hover:shadow-lg hover:bg-black focus:ring-4 focus:ring-neutral-tertiary shadow-lg font-medium leading-5 rounded-lg text-md px-4 py-2.5 focus:outline-none">
             {loading ? <LoaderAnim/> : "Sign Up"}
         </button>
 
